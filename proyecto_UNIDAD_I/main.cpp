e65d022a3e39cdcf588ded446a91eb2a65192842
Adely Perez <adelyalvla@gmail.com>
2021-02-20 01:20:29 -0600

    Subiendo proyecto_unidadI

--- proyecto_UNIDAD_I/main.cpp
+++ proyecto_UNIDAD_I/main.cpp
@@ -1,3 +1,4 @@
+//Librerias
 #include <iostream>
 #include "startShip.h"
 #include "snake.h"
@@ -6,21 +7,25 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    //MENU PRINCIPAL DEL PROGRAMA
-    int opcion;
-    system("cls");
-    cout << "******************" << endl;
-    cout << "  MENU DE JUEGOS  " << endl;
-    cout << "******************" << endl;
-    cout << endl;
-    cout << "Seleccione un Juego\n";
-    cout << "1 - StarShip" << endl;
-    cout << "2 - Snake" << endl;
-    cout << "Ingrese un numero del menu para iniciar un juego: ";
-    cin >> opcion;
+    bool salir = false;
+
+    while(salir == false){
+        //MENU PRINCIPAL DEL PROGRAMA
+        int opcion;
+        system("cls");
+        cout << "******************" << endl;
+        cout << "  MENU DE JUEGOS  " << endl;
+        cout << "******************" << endl;
+        cout << endl;
+        cout << "Seleccione un Juego\n";
+        cout << "1 - StarShip" << endl;
+        cout << "2 - Snake" << endl;
+        cout << "3 - SALIR" << endl;
+        cout << "Ingrese un numero del menu para iniciar un juego: ";
+        cin >> opcion;
     
-    //En caso de que se elija una opcion, mostrará uno de los dos juegos
-    switch(opcion){
+        //En caso de que se elija una opcion, mostrará uno de los dos juegos
+        switch(opcion){
         
         case 1:
             starShip();
@@ -30,8 +35,14 @@ int main(int argc, char const *argv[])
             snake();
             break;
 
+        case 3:
+            salir = true;
+            break;
+
         default:
             break;
+        }
     }
+    
     return 0;
 }
